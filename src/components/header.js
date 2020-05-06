22aa7976e1ddec88e6164b1c05e93586e3407ad4
jithish-sekar <63664628+jithish-sekar@users.noreply.github.com>
2020-05-06 17:48:18 +0530

    header

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 class HeaderComponent extends Component {
     home() {
-      this.props.history.push('/home')
+        this.props.history.push('/home')
     }
     work() {
         this.props.history.push('/work')
@@ -20,25 +20,27 @@ class HeaderComponent extends Component {
                 <nav className='headernav'>
                     <div>
                         <h1 className='built'>BUILT BY</h1>
-                        <span
-                            onClick={()=>this.home()}
-                            className='headernavspan'
-                        >Home</span>
-                        <span
-                            onClick={()=>this.work()}
-                            className='headernavspan'
-                        >Work</span>
-                        <img
-                            className='headerlogo'
-                            src='logo1.png' />
-                        <span
-                            onClick={()=>this.who()}
-                            className='headernavspan'
-                        >Who</span>
-                        <span
-                            onClick={()=>this.about()}
-                            className='headernavspan'
-                        >About</span>
+                        <div className='hdr'>
+                            <span
+                                onClick={() => this.home()}
+                                className='headernavspan'
+                            >Home</span>
+                            <span
+                                onClick={() => this.work()}
+                                className='headernavspan'
+                            >Work</span>
+                            <img
+                                className='headerlogo'
+                                src='logo1.png' />
+                            <span
+                                onClick={() => this.who()}
+                                className='headernavspan'
+                            >Who</span>
+                            <span
+                                onClick={() => this.about()}
+                                className='headernavspan'
+                            >About</span>
+                        </div>
                     </div>
                 </nav>
             </div>
