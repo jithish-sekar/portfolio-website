6cd0639b5bec3de026577d3751da06da52d61eee
jithish-sekar <63664628+jithish-sekar@users.noreply.github.com>
2020-05-06 18:57:47 +0530

    who

--- src/components/who.js
+++ src/components/who.js
@@ -14,7 +14,20 @@ class Who extends Component {
                         <p className='team'>Meet the team!</p>
                      </div>
                     <h1 className='strapline'></h1>
+                        <br/>
+                    <img
+                        className='pic'
+                        src='jithu2.jpg' />
                 </center>
+                <p className='nam1'>JITHISH SEKAR</p>
+                <p className='nam2'>(FULL STACK WEB DEVELOPER)</p>
+                <div className=''>
+                        <a href='' className='link'><i class="fa fa-facebook-square"></i></a>
+                        <a href='' className='link'><i class="fa fa-linkedin-square"></i></a>
+                        <a href='' className='link'><i class="fa fa-github-square"></i></a>
+                        <a href='' className='link'><i class="fa fa-behance-square"></i></a>
+                        <a href='' className='link'><i class="fa fa-twittere-square"></i></a>
+                    </div>
             </div>
 
         )
