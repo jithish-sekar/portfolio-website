fa50bfc0cf6bb345a07f57f06ed41d85d2f16852
jithish-sekar <63664628+jithish-sekar@users.noreply.github.com>
2020-05-08 15:53:20 +0530

    img

--- src/components/work.js
+++ src/components/work.js
@@ -13,9 +13,10 @@ class Work extends Component {
                         <img
                             className='rob'
                             src='robot.png' />
-                        <h1 className='workh1'>We put our hearts and souls into making every project we undertake the very best it can possibly be – and we don’t stop until we get there.</h1>
                     </center>
-                    <center><h1 className='medikare1'>Some of Studio Bulls latest projects</h1></center>
+                    <center><h1 className='medikare1'>Some of Studio Bulls latest projects</h1>
+                    <h1 className='workh1'>We put our hearts and souls into making every project we undertake the very best it can possibly be – and we don’t stop until we get there.</h1>
+                    </center>
                     <h1 className='medi'>medikare.org.in</h1>
                     <p className='workp'>(Brand and information site)</p>
                     < center> <img className='workimg' src='medikare.jpg' /></center>
