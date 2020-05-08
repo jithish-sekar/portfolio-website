8f9df4a254acf44dec714de088ab3ae785006c9a
jithish-sekar <63664628+jithish-sekar@users.noreply.github.com>
2020-05-08 16:35:34 +0530

    Update who.js

--- src/components/who.js
+++ src/components/who.js
@@ -27,11 +27,11 @@ class Who extends Component {
                     <p className='nam1'>JITHISH SEKAR</p>
                     <p className='nam2'>(FULL STACK WEB DEVELOPER)</p>
                     <div className='wholink'>
-                        <a href='' className='link'><i class="fa fa-facebook-square"></i></a>
-                        <a href='' className='link'><i class="fa fa-linkedin-square"></i></a>
-                        <a href='' className='link'><i class="fa fa-github-square"></i></a>
-                        <a href='' className='link'><i class="fa fa-behance-square"></i></a>
-                        <a href='' className='link'><i class="fa fa-twittere-square"></i></a>
+                        <a href='https://www.linkedin.com/in/jithish-sekar-a46a95157?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3Bh8jOfTQUSlWqq9PhJ2Y2Wg%3D%3D' className='link'><i class="fa fa-linkedin-square"></i></a>
+                        <a href='https://github.com/jithish-sekar' className='link'><i class="fa fa-github-square"></i></a>
+                        <a href='https://www.behance.net/jithusekar' className='link'><i class="fa fa-behance-square"></i></a>
+                        <a href='https://www.facebook.com/profile.php?id=100012662224779' className='link'><i class="fa fa-facebook-square"></i></a>
+                        <a href='https://twitter.com/JithuSekar' className='link'><i class="fa fa-twitter-square"></i></a>
                     </div>
                 </div>
                 <br /><br />
@@ -44,11 +44,11 @@ class Who extends Component {
                     <p className='nam1'>DHANUSH SUNDARAJAN</p>
                     <p className='nam2'>(GRAPHIC DESIGNER)</p>
                     <div className='wholink'>
-                        <a href='' className='link'><i class="fa fa-facebook-square"></i></a>
-                        <a href='' className='link'><i class="fa fa-linkedin-square"></i></a>
-                        <a href='' className='link'><i class="fa fa-github-square"></i></a>
-                        <a href='' className='link'><i class="fa fa-behance-square"></i></a>
-                        <a href='' className='link'><i class="fa fa-twittere-square"></i></a>
+                        <a href='https://www.linkedin.com/in/dhanush-s-b125491a7/' className='link'><i class="fa fa-linkedin-square"></i></a>
+                        <a href='https://github.com/dhanush2206' className='link'><i class="fa fa-github-square"></i></a>
+                        <a href='https://www.behance.net/dhanushs1' className='link'><i class="fa fa-behance-square"></i></a>
+                        <a href='https://www.facebook.com/dhanush.sundararajan' className='link'><i class="fa fa-facebook-square"></i></a>
+                        <a href='https://twitter.com/dhan619?s=08' className='link'><i class="fa fa-twitter-square"></i></a>
                     </div>
                 </div>
                 <br /><br />
