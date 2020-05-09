cee3f314e6c2a8b2a986d167eb78baaa4488a9c7
jithish-sekar <63664628+jithish-sekar@users.noreply.github.com>
2020-05-09 19:57:49 +0530

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -22,6 +22,7 @@ class Footerpage extends Component {
                
                     <br /> */}
                 <div className='linkfb'>
+                    
                     <a href='https://www.linkedin.com/in/jithish-sekar-a46a95157?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3Bh8jOfTQUSlWqq9PhJ2Y2Wg%3D%3D' className='link'><i class="fa fa-linkedin-square"></i></a>
                     <a href='https://www.dribbble.com/dhanush-s' className='link'>  <i class="fa fa-dribbble"></i></a>
                     <a href='https://www.behance.net/dhanushs1' className='link'><i class="fa fa-behance-square"></i></a>
@@ -29,6 +30,7 @@ class Footerpage extends Component {
                     <a href='https://www.facebook.com/dhanush.sundararajan' className='link'><i class="fa fa-facebook-square"></i></a>
                     <a href='https://twitter.com/dhan619?s=08' className='link'><i class="fa fa-twittere-square"></i></a>
                 </div>
+            
                 <span
                     onClick={() => this.home()}
                     className='ftrp'>Home</span>
